32984724f2e341c011417597e3833b9743719878
chocolateboy <chocolate@cpan.org>
2021-04-16 13:49:26 +0100

    pre-empt the function rather than replacing it

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function rwt () { return true } // same return value as the original
 
-if (window.wrappedJSObject.rwt) {
-    // assign it as a read-only property to prevent it being overridden
-    Object.defineProperty(window.wrappedJSObject, 'rwt', {
-        value: exportFunction(rwt, window),
-        writable: false,
-        configurable: false,
-    })
-}
+// assign it as a read-only property to prevent it being overridden
+Object.defineProperty(window.wrappedJSObject, 'rwt', {
+    value: exportFunction(rwt, window),
+    writable: false,
+    configurable: false,
+})
